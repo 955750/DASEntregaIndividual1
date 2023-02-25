0a5e803c6888b8484dbd57de695671cb197cdc22
955750 <019jfuentes@gmail.com>
2023-02-25 14:10:11 +0100

    Lista de jornadas con 'RecyclerView' (HECHO)

--- app/src/main/java/com/example/dasentregaindividual1/jornada/ListaJornadasAdapter.java
+++ app/src/main/java/com/example/dasentregaindividual1/jornada/ListaJornadasAdapter.java
@@ -14,32 +14,40 @@ public class ListaJornadasAdapter extends RecyclerView.Adapter<ListaJornadasView
     private int[] escudosEquiposLocales;
     private String[] nombresEquiposLocales;
     private String[] ultimosPartidosEquiposLocales;
+    private int[] puntosEquiposLocales;
 
     private int[] escudosEquiposVisitantes;
     private String[] nombresEquiposVisitantes;
     private String[] ultimosPartidosEquiposVisitantes;
+    private int[] puntosEquiposVisitantes;
 
     public ListaJornadasAdapter(
         int[] pEscudosEquiposLocales,
         String[] pNombresEquiposLocales,
         String[] pUltimosPartidosEquiposLocales,
+        int[] pPuntosEquiposLocales,
         int[] pEscudosEquiposVisitantes,
         String[] pNombresEquiposVisitantes,
-        String[] pUltimosPartidosEquiposVisitantes
+        String[] pUltimosPartidosEquiposVisitantes,
+        int[] pPuntosEquiposVisitantes
     ) {
         escudosEquiposLocales = pEscudosEquiposLocales;
         nombresEquiposLocales = pNombresEquiposLocales;
         ultimosPartidosEquiposLocales = pUltimosPartidosEquiposLocales;
+        puntosEquiposLocales = pPuntosEquiposLocales;
+
         escudosEquiposVisitantes = pEscudosEquiposVisitantes;
         nombresEquiposVisitantes = pNombresEquiposVisitantes;
         ultimosPartidosEquiposVisitantes = pUltimosPartidosEquiposVisitantes;
+        puntosEquiposVisitantes = pPuntosEquiposVisitantes;
     }
 
     @NonNull
     @Override
     public ListaJornadasViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View listaJornadasItem= LayoutInflater
-                .from(parent.getContext()).inflate(R.layout.lista_jornadas_item,null);
+                .from(parent.getContext())
+                .inflate(R.layout.lista_jornadas_item, parent, false);
         ListaJornadasViewHolder listaJornadasViewHolder = new ListaJornadasViewHolder(
                 listaJornadasItem);
         return listaJornadasViewHolder;
@@ -50,10 +58,12 @@ public class ListaJornadasAdapter extends RecyclerView.Adapter<ListaJornadasView
         holder.escudoEquipoLocalIV.setImageResource(escudosEquiposLocales[position]);
         holder.nombreEquipoLocalTV.setText(nombresEquiposLocales[position]);
         holder.ultimosPartidosEquipoLocalTV.setText(ultimosPartidosEquiposLocales[position]);
+        holder.puntosEquipoLocalTV.setText(String.valueOf(puntosEquiposLocales[position]));
 
         holder.escudoEquipoVisitanteIV.setImageResource(escudosEquiposVisitantes[position]);
         holder.nombreEquipoVisitanteTV.setText(nombresEquiposVisitantes[position]);
         holder.ultimosPartidosEquipoVisitanteTV.setText(ultimosPartidosEquiposVisitantes[position]);
+        holder.puntosEquipoVisitanteTV.setText(String.valueOf(puntosEquiposVisitantes[position]));
     }
 
     @Override
